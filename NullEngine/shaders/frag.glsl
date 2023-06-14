b4505ae0a0232e62adada6b42401c502ca1671b3
Texe1 <73749424+Texe1@users.noreply.github.com>
2023-06-14 12:03:00 +0200

    Colorful Triangle

--- NullEngine/shaders/frag.glsl
+++ NullEngine/shaders/frag.glsl
@@ -1,7 +1,8 @@
 #version 450 core
 
 layout(location = 0) out vec4 color_out;
+layout(location = 0) in vec3 col;
 
 void main() {
-	color_out = vec4(1.0, 1.0, 1.0, 1.0); 
+	color_out = vec4(col, 1.0); 
 }
