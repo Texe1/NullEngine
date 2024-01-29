29d8f201808f8a777ce57e0cab96785a591bfef1
Felix Bomm <bomm.felix@gmail.com>
2024-01-29 11:25:14 +0100

    typedefs

---
+++ src/include/typedef.h
@@ -0,0 +1,19 @@
+#pragma once
+
+typedef unsigned char 		u8;
+typedef unsigned short 		u16;
+typedef unsigned int 		u32; 
+typedef unsigned long long 	u64;
+
+typedef char 				i8;
+typedef short 				i16;
+typedef int 				i32;
+typedef long long 			i64;
+
+// it is recommended to use decimal for most cases, as it will stay consistent with NullEngine in future updates
+typedef float 				f32;
+// it is recommended to use decimal for most cases, as it will stay consistent with NullEngine in future updates
+typedef double				f64;
+
+// the default floating-point type (32-bit/single-precision)
+typedef f32					decimal;
