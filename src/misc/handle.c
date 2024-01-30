052d44b223926f9fc45a978c179aade892ef0752
Felix Bomm <bomm.felix@gmail.com>
2024-01-30 18:35:58 +0100

    completed _init_handle_table function

--- src/misc/handle.c
+++ src/misc/handle.c
@@ -7,8 +7,14 @@ void* _init_handle_table(u64 _n, void* _ptr){
 	struct _handle_table* table = _ptr;
 	void* data = table + 1;
 
-	memset(data, _n, sizeof(struct _handle_table_entry));
+	memset(data, 0, _n * sizeof(struct _handle_table_entry));
 
-	//TODO
-	return 1;
+	table->nFree = _n;
+	*table = (struct _handle_table) {
+		.nFree = _n,
+		.nUsed = 0,
+		.nMax = _n,
+	};
+	
+	return ((char*)data) + _n * sizeof(struct _handle_table_entry);
 }
