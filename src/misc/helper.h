fa0044bab91d03e8599525ef939b781a9cfc80a3
Felix Bomm <bomm.felix@gmail.com>
2024-01-30 22:15:04 +0100

    helper.h now only resolves macros to actual code when _DEBUG is defined

--- src/misc/helper.h
+++ src/misc/helper.h
@@ -1,6 +1,16 @@
 #pragma once
 #include <Windows.h>
 
+#ifdef _DEBUG
+
 #define LOG(x) OutputDebugString(x)
 #define DBG(x) LOG(x);DebugBreak()
 #define BRK DBG("Breakpoint Occured\n")
+
+#else
+
+#define LOG(x)
+#define DBG(x)
+#define BRK 
+
+#endif
