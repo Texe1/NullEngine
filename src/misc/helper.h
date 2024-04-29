9bcbb214752a203181133e568ed645a326dde868
Felix Bomm <bomm.felix@gmail.com>
2024-04-29 23:47:53 +0200

    "Better" Logging

--- src/misc/helper.h
+++ src/misc/helper.h
@@ -3,13 +3,18 @@
 
 #ifdef _DEBUG
 
-#define LOG(x) OutputDebugString(x)
-#define DBG(x) LOG(x);DebugBreak()
+#define DBG(x) OutputDebugString(x);DebugBreak()
 #define BRK DBG("Breakpoint Occured\n")
+
+#define LOG(x) DBG(__FUNCSIG__ ": " x)
+#define WRN(x) DBG(__FUNCSIG__ ": Warning: " x)
+#define ERR(x) DBG(__FUNCSIG__ ": Error: " x)
 
 #else
 
 #define LOG(x)
+#define WRN(x)
+#define ERR(x)
 #define DBG(x)
 #define BRK 
 
